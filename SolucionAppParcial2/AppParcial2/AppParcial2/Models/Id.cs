8d159d02316393aa8db6b68c880a6d1363066c5f
Mitch <Yerko@DESKTOP-L1AAA5D>
2020-06-13 10:57:43 -0400

    l

---
+++ SolucionAppParcial2/AppParcial2/AppParcial2/Models/Id.cs
@@ -0,0 +1,24 @@
+﻿namespace AppParcial2.Models
+
+{
+    using System;
+    using Newtonsoft.Json;
+
+    public partial class Id
+    {
+        [JsonProperty("albumId")]
+        public long AlbumId { get; set; }
+
+        [JsonProperty("id")]
+        public long IdId { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("url")]
+        public Uri Url { get; set; }
+
+        [JsonProperty("thumbnailUrl")]
+        public Uri ThumbnailUrl { get; set; }
+    }
+}
